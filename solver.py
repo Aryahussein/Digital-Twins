114731027e30fd453d91b45b5d1013fcda0b0c50
AlexanderLivGithub <100223889+AlexanderLivGithub@users.noreply.github.com>
2026-01-27 12:19:03 -0600

    Added ability to retrieve super-LU matrix

--- solver.py
+++ solver.py
@@ -1,4 +1,18 @@
 from scipy.sparse.linalg import spsolve
+from scipy.sparse.linalg import splu
+
 
 def solve_sparse(G, I):
     return spsolve(G, I)
+
+def lu_factorize(Y):
+    """
+    Returns a super-LU object. Individual L and U matrices
+    can be accessed as lu.L and lu.U, but is not encouraged.
+    To use the transpose of the super-LU object,
+    v = lu.solve(I, trans='T').
+    :param Y:
+    :return:
+    """
+    lu = splu(Y)
+    return lu
